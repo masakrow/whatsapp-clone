0b479dac2e9a9a79afe81185e5d6996f48b7cf53
masakrow <michalc00@gmail.com>
2020-08-19 21:52:38 +0200

    Fist commit Added Sidebar Component and install Material-UI

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCo8ntUEoTqRdePPFBRNRLOa703fzMyngI",
+  authDomain: "whatsapp-clone-b86d0.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-b86d0.firebaseio.com",
+  projectId: "whatsapp-clone-b86d0",
+  storageBucket: "whatsapp-clone-b86d0.appspot.com",
+  messagingSenderId: "190746877811",
+  appId: "1:190746877811:web:568e0c0d1cc30ba8076f06",
+};
