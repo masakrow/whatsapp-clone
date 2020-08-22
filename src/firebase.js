b2a80518cf4df74efeb74bc1b5c873281d555744
masakrow <michalc00@gmail.com>
2020-08-22 19:51:30 +0200

    Added DB in firebase called "rooms". Added functionality to add new rooms in SidebarChat. Integration with Database in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCo8ntUEoTqRdePPFBRNRLOa703fzMyngI",
   authDomain: "whatsapp-clone-b86d0.firebaseapp.com",
@@ -7,3 +9,12 @@ const firebaseConfig = {
   messagingSenderId: "190746877811",
   appId: "1:190746877811:web:568e0c0d1cc30ba8076f06",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
